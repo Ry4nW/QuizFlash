f53d747ff32e6d1ffaad6eeb86a1f800d3877374
starrydash <ryclowe@gmail.com>
2023-01-21 15:50:44 -0800

    Removed old site prototype

--- quiz-flash/ask_question.py
+++ quiz-flash/ask_question.py
@@ -1,6 +1,9 @@
 import random
 
 def askquestion(questions):
-    asked_q = random.choice(questions)
+    while True:
+        asked_q = random.choice(questions)
+        if asked_q.tag_check() or asked_q.tags == []:
+            break
     asked_q.ask()
     return asked_q
--- quiz-flash/main.py
+++ quiz-flash/main.py
@@ -1,11 +1,16 @@
 import random, ask_question
 
+# import data from the database
 data = []
 
+def correct():
+    points += 1
+
 class Question:
-    def __init__(self,question,answer):
+    def __init__(self,question,answer,tags):
         self.question = question
         self.answer = answer
+        self.tags = tags
 
     def ask(self):
         print(f"Question: {self.question}")
@@ -15,9 +20,19 @@ class Question:
         else:
             print(f"Wrong, the answer was {answer}")
 
+    def tag_check(self,tag):
+        if not tag in self.tags:
+            return False
+            correct()
+        else:
+            return True
+
 questions = []
 for q in data:
-    questions.append(Question(q[0],q[1])) # dependant on the question
+    questions.append(Question(q[0],q[1],q[2])) # dependant on the question
+    # q[0] = Question
+    # q[1] = Answer
+    # q[2] = Tags (Subject, Difficulty, etc.)
 
 askedquestion = ask_question.askquestion(questions)
 questions.remove(askedquestion)
