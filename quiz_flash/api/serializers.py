fb65bb35c042e13bb8434886212fb1d546056567
Gordon Zhou <gordonzhou223@gmail.com>
2023-01-21 18:20:07 -0800

    add create room view

--- quiz_flash/api/serializers.py
+++ quiz_flash/api/serializers.py
@@ -4,4 +4,9 @@ from .models import Room
 class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
-        fields = ('id', 'code', 'host', 'created_at', 'round_time_limit')
+        fields = ('id', 'code', 'host', 'created_at', 'round_time_limit', 'max_players')
+
+class CreateRoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ('round_time_limit', 'max_players')
--- quiz_flash/api/views.py
+++ quiz_flash/api/views.py
@@ -1,12 +1,43 @@
 from django.shortcuts import render
-from rest_framework import generics
-from .serializers import RoomSerializer
+from rest_framework import generics, status
+from .serializers import RoomSerializer, CreateRoomSerializer
 from .models import Room
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
 class RoomView(generics.CreateAPIView):
     query = Room.objects.all()
     serializer_class = RoomSerializer
+
+class CreateRoomView(APIView):
+    serializer_class = CreateRoomSerializer
+
+    def post(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            round_time_limit = serializer.data.get('round_time_limit')
+            max_players = serializer.data.get('max_players')
+            host = self.request.session.session_key
+            queryset = Room.objects.filter(host=host)
+            if queryset.exists():
+                room = queryset[0]
+                room.round_time_limit = round_time_limit
+                room.max_players = max_players
+                room.save(update_fields=['round_time_limit', 'max_players'])
+            else:
+                room = Room(
+                    host=host, 
+                    round_time_limit=round_time_limit,
+                    max_players=max_players
+                )
+                room.save()
+    
+        return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
 
 def front(request):
     context = { }
