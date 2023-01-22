9e0e2aa16bf1c9d750c7e551ed5f635d3bfbd4df
starrydash <ryclowe@gmail.com>
2023-01-22 09:30:05 -0800

    Link or smth??

--- quiz_flash/frontend/src/Header.js
+++ quiz_flash/frontend/src/Header.js
@@ -1,18 +1,18 @@
 import React from "react";
-import {Link} from 'react-router-dom';
+import {a} from 'react-router-dom';
 import "./Header.css"
 
 function Header(props) {
   return (
     <nav>
-      <link rel="stylesheet" href="../../style.css"/>
+      <a rel="stylesheet" href="../../style.css"/>
         <div className="title_container">
           <h1 className="title_text">QuizFlash</h1>
         </div>
         <ul className="navbar-main">
-            <li className="nav-ele nav_text"><Link to="App.js">Home</Link></li>
+            <li className="nav-ele nav_text"><a to="App.js">Home</a></li>
             <li className="nav-ele nav_text"><a href="/">Your library</a></li>
-            <li className="nav-ele nav_text"><Link to="Lobby.js">Create</Link></li>
+            <li className="nav-ele nav_text"><a to="Lobby.js">Create</a></li>
         </ul>
     </nav>
   );
