1d1bf22363e45407f8f7f49f61c6fabb72906fc3
Gordon Zhou <gordonzhou223@gmail.com>
2023-01-21 23:45:49 -0800

    move frontend to quiz_flash

--- frontend/ask_question.py
+++
@@ -1,9 +0,0 @@
-import random
-
-def askquestion(questions):
-    while True:
-        asked_q = random.choice(questions)
-        if asked_q.tag_check() or asked_q.tags == []:
-            break
-    asked_q.ask()
-    return asked_q
