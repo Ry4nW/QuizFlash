1fcf1afbb366bf1018507e8a266d5609add2b520
Gordon Zhou <gordonzhou223@gmail.com>
2023-01-23 12:53:15 -0800

    update database models

--- quiz_flash/api/urls.py
+++ quiz_flash/api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import RoomView, CreateRoomView, GetRoom, JoinRoom
+from .views import *
 
 urlpatterns = [
     path('home', RoomView.as_view()),
     path('create-room', CreateRoomView.as_view()),
     path('get-room', GetRoom.as_view()),
-    path('join-room', JoinRoom.as_view()),
+    path('jwer-flash-card', AnswerFlashCard.as_view()),
+    path('oin-room', JoinRoom.as_view()),
+    path('ansend-round', EndRound.as_view())
 ]
--- quiz_flash/api/views.py
+++ quiz_flash/api/views.py
@@ -1,44 +1,23 @@
 from django.shortcuts import render
 from rest_framework import generics, status
-from .serializers import RoomSerializer, CreateRoomSerializer
-from .models import Room
+from .serializers import RoomSerializer, CreateRoomSerializer, PlayerSerializer
+from .models import Room, Player
 from rest_framework.views import APIView
 from rest_framework.response import Response
+import datetime
 
-# Create your views here.
 class RoomView(generics.CreateAPIView):
     query = Room.objects.all()
     serializer_class = RoomSerializer
 
 class JoinRoom(APIView):
-    def post(self, request, format=None):
-        if not self.request.session.exists(self.request.session.session_key):
-            self.request.session.create()
-        
-class GetRoom(APIView):
-    serializer_class = RoomSerializer
-    code_key = 'code'
-
-    def get(self, request, format=None):
-        code = request.GET.get(self.code_key)
-        if code != None:
-            room = Room.objects.filter(code=code)
-            if len(room) > 0:
-                data = RoomSerializer(room[0]).data
-                data['is_host'] = self.request.session.session_key == room[0].host
-                return Response(data, status=status.HTTP_200_OK)
-            return Response({'Room Not Found': 'Invalid Room Code.'}, status=status.HTTP_404_NOT_FOUND)
-
-        return Response({'Bad Request': 'Code paramater not found in request'}, status=status.HTTP_400_BAD_REQUEST)
-
-class JoinRoom(APIView):
     code_key = 'code'
 
     def post(self, request, format=None):
         if not self.request.session.exists(self.request.session.session_key):
             self.request.session.create()
 
-        code = request.data.get(self.lookup_url_kwarg)
+        code = request.data.get(self.code_key)
         if code != None:
             room_result = Room.objects.filter(code=code)
             if len(room_result) > 0:
@@ -46,9 +25,75 @@ class JoinRoom(APIView):
                 self.request.session['room_code'] = code
                 return Response({'message': 'Room Joined!'}, status=status.HTTP_200_OK)
 
-            return Response({'Bad Request': 'Invalid Room Code!'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'Bad Request': 'Invalid Room Code'}, status=status.HTTP_400_BAD_REQUEST)
 
-        return Response({'Bad Request': 'Invalid post data'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({'Bad Request': 'Invalid post data, did not find a code key'}, status=status.HTTP_400_BAD_REQUEST)
+
+class EndRound(APIView):
+    code_key = 'code'
+
+    def post(self, request, format=None):
+        code = request.GET.get(self.code_key)
+        room = Room.objects.filter(code=code)
+        if len(room) > 0:
+            room = room[0]
+            queryset = Player.objects.filter(room__code=code)
+
+    
+        return Response({'Bad Request': 'Invalid Room Code'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class AnswerFlashCard(APIView):
+    serializer_class = PlayerSerializer
+    code_key = 'code'
+    name_key = 'name'
+    answer_key = 'answer'
+
+    def post(self, request, format=None):
+        code = request.GET.get(self.code_key)
+        name = request.GET.get(self.name_key)
+        player = Player.objects.filter(name=name).filter(room__code=code)
+        if player.exists():
+            player = player[0]
+        else:
+            return Response({}, status=status.HTTP_404_NOT_FOUND)
+
+        my_answer = request.GET.get(self.answer)
+        room = Room.objects.filter(code=code)
+        quiz = room[0].quiz
+        true_answer = quiz[room[0].rounds]
+        data = RoomSerializer(room[0]).data
+        data['correct'] = my_answer == true_answer
+        player.submit_time = datetime.datetime.utcnow()
+        player.save(update_fields=['submit_time'])
+
+        return Response(data, status=status.HTTP_200_OK)
+
+        
+class GetRoom(APIView):
+    serializer_class = RoomSerializer
+    code_key = 'code'
+    name_key = 'name'
+
+    def get(self, request, format=None):
+        code = request.GET.get(self.code_key)
+        name = request.GET.get(self.name_key)
+        if code != None:
+            room = Room.objects.filter(code=code)
+            if len(room) > 0:
+                data = RoomSerializer(room[0]).data
+                data['is_host'] = self.request.session.session_key == room[0].host
+                player = Player(
+                    user_score=0,
+                    name=name,
+                    room=room[0]
+                )
+                player.save()
+                return Response(data, status=status.HTTP_200_OK)
+            return Response({'Room Not Found': 'Invalid Room Code.'}, status=status.HTTP_404_NOT_FOUND)
+
+        return Response({'Bad Request': 'Code paramater not found in request'}, status=status.HTTP_400_BAD_REQUEST)
 
 class CreateRoomView(APIView):
     serializer_class = CreateRoomSerializer
@@ -80,7 +125,3 @@ class CreateRoomView(APIView):
                 self.request.session['room_code'] = room.code
     
         return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
-
-def front(request):
-    context = { }
-    return render(request, "index.html", context)
